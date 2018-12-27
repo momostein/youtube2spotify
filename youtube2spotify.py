fe0b31737893d45ba23924db60ed57dea11b3128
Momostein <brecht.ooms@hotmail.com>
2018-12-27 15:27:36 +0100

    socketio now works, but youtube api broke

--- youtube2spotify.py
+++ youtube2spotify.py
@@ -1,18 +1,28 @@
 # Main flask app
+import eventlet
+eventlet.monkey_patch()
+
+import time
+from threading import Thread
 
 import flask
+from flask_socketio import SocketIO, emit, join_room, disconnect
+import os
+
 import secret_key
 import youtube
 import spotify
 
 app = flask.Flask(__name__)
-
 app.secret_key = secret_key.get(True)
 
 app.register_blueprint(youtube.youtube)
 app.register_blueprint(spotify.spotifyBP)
-
 spotify.oauth.init_app(app)
+
+socketio = SocketIO(app)
+
+thread = None
 
 
 @app.route("/")
@@ -20,5 +30,23 @@ def index():
     return "Index page"
 
 
+@app.route('/socket')
+def socket():
+    global thread
+    if thread is None:
+        thread = Thread(target=background_stuff)
+        thread.start()
+    return flask.render_template('socket.html')
+
+
+def background_stuff():
+    print('In background_stuff')
+    while True:
+        time.sleep(1)
+        t = str(time.process_time())
+        socketio.emit('message', {'data': 'this is data', 'time': t}, namespace='/test')
+
+
 if __name__ == "__main__":
-    app.run(host='localhost', port=8090, debug=True, ssl_context='adhoc')
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    socketio.run(app, host='localhost', port=8090, debug=True)
